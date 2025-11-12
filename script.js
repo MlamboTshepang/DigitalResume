3fa4bcd9ca72d4963ee1499ebba350ae608ea890
MlamboTshepang <Tshepangmlambo@gmail.com>
2025-11-12 17:55:55 +0200

    Changing Logo
    Changing the logo

--- script.js
+++ script.js
@@ -4,11 +4,9 @@ document.addEventListener('DOMContentLoaded', function(){
     // Moving Text
     const words = ["Front End Developer", "Back End Developer", "Full Stack Developer", "UI Designer"];
     const textElement = document.querySelector(".typing-text");
-
     let wordIndex = 0;  // Which word we are on
     let letterIndex = 0; // Which letter of that word we are on
     let isDeleting = false;
-
 // Speeds (in milliseconds)
     const typeSpeed = 150;
     const deleteSpeed = 100;
